e744f0f5199ab4701fea17377e89558c95615c3e
Andrej <gajduk.andrej@gmail.com>
2017-09-27 18:33:48 +0200

    removed targetClass use parentDom attribute instead

--- src/PerfectScrollbar/widget/PerfectScrollbar.js
+++ src/PerfectScrollbar/widget/PerfectScrollbar.js
@@ -2,6 +2,7 @@ define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
 
+    "mendix/lang",
     "mxui/dom",
     "dojo/dom",
     "dojo/dom-prop",
@@ -15,91 +16,62 @@ define([
     "dojo/html",
     "dojo/query",
     "dojo/_base/event",
-    "PerfectScrollbar/widget/perfect-scrollbar.min",
-    "dojo/text!PerfectScrollbar/widget/ui/perfect-scrollbar.min.css"
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml,
+    "PerfectScrollbar/widget/perfect-scrollbar.min"
+], function (declare, _WidgetBase, mxlang, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml,
       dojoQuery, dojoEvent, Ps) {
     "use strict";
 
     return declare("PerfectScrollbar.widget.PerfectScrollbar", [ _WidgetBase ], {
 
         //widget parameters
-        targetClass: "",
         scrollToAttribute: "",
 
         // Internal variables.
         _handles: null,
         _contextObj: null,
 
+        _container: null,
+
         constructor: function () {
             this._handles = [];
         },
 
-        postCreate: function () {
-            logger.debug(this.id + ".postCreate");
-        },
-
         update: function (obj, callback) {
-            logger.debug(this.id + ".update");
-
             this._contextObj = obj;
             this._updateRendering(callback);
         },
 
-        resize: function (box) {
-          logger.debug(this.id + ".resize");
+        uninitialize: function () {
         },
 
-        uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
+        _activateScrollBar: function() {
+          var self = this;
+          if ( self._container.classList.contains('ps') ) {
+            Ps.update(self._container);
+          }
+          else {
+            Ps.initialize(self._container);
+          }
+          if ( self._contextObj && self._contextObj.get(self.scrollToAttribute) )
+             self._scrollToMe(self._container, + (self._contextObj.get(self.scrollToAttribute)+''));
         },
 
         _updateRendering: function (callback) {
-            logger.debug(this.id + "._updateRendering");
-
-
-            var container = dojoQuery("."+this.targetClass)[0];
-            if ( container ) {
-              Ps.initialize(container);
-              if ( this._contextObj && this._contextObj.get(this.scrollToAttribute) )
-                this._scrollToMe(container, + (this._contextObj.get(this.scrollToAttribute)+''));
-            }
-            this._executeCallback(callback, "_updateRendering");
+            var self = this;
+            self._container = self.domNode.parentNode;
+            self._activateScrollBar();
+            self._executeCallback(callback, "_updateRendering");
         },
 
         _scrollToMe: function(container, offset) {
           var self = this;
           if ( offset == 0 ) return;
           container.scrollTop = offset;
-          if ( container.scrollTop == 0 )
-            setTimeout(function () { self._scrollToMe(container, offset); } ,50)
           Ps.update(container);
-        },
-
-        // Shorthand for running a microflow
-        _execMf: function (mf, guid, cb) {
-            logger.debug(this.id + "._execMf");
-            if (mf && guid) {
-                mx.ui.action(mf, {
-                    params: {
-                        applyto: "selection",
-                        guids: [guid]
-                    },
-                    callback: lang.hitch(this, function (objs) {
-                        if (cb && typeof cb === "function") {
-                            cb(objs);
-                        }
-                    }),
-                    error: function (error) {
-                        console.debug(error.description);
-                    }
-                }, this);
-            }
         },
 
         // Shorthand for executing a callback, adds logging to your inspector
         _executeCallback: function (cb, from) {
-            logger.debug(this.id + "._executeCallback" + (from ? " from " + from : ""));
             if (cb && typeof cb === "function") {
                 cb();
             }
